357c9d1b8b03312f2c76b85faa013dc450e0db1f
Henry Scheible <henry.scheible@gmail.com>
2019-08-12 19:35:55 -0500

    Change contents of model, no real code changes

--- dataview/model.py
+++ dataview/model.py
@@ -6,8 +6,8 @@ class Model(Gtk.ListStore):
 
     def __init__(self):
         super(Model, self).__init__(str, str)
-        self.append(["Henry", "Child"])
-        self.append(["Jennifer", "Child"])
-        self.append(["Sophie", "Child"])
-        self.append(["Paul", "Child"])
-        self.columns = ["Name", "Role"]
+        self.append(["Example", "Example"])
+        self.append(["Example", "Example"])
+        self.append(["Example", "Example"])
+        self.append(["Example", "Example"])
+        self.columns = ["ExampleProperty", "ExampleProperty2"]
